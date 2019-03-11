762aaee41b617f7a16753b7447f936529334a0ed
Van Quan <tieuvuongtu97.ie@gmail.com>
2019-03-11 16:46:05 +0700

    up frame

---
+++ QL_Baixe_app/frLogin.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace QL_Baixe_app
+{
+    partial class frLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.lbTrangthai = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btDangNhap = new System.Windows.Forms.Button();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ckbShowPass = new System.Windows.Forms.CheckBox();
+            this.txbUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gbLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(265, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            // 
+            // gbLogin
+            // 
+            this.gbLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.gbLogin.Controls.Add(this.lbTrangthai);
+            this.gbLogin.Controls.Add(this.checkBox1);
+            this.gbLogin.Controls.Add(this.button1);
+            this.gbLogin.Controls.Add(this.btDangNhap);
+            this.gbLogin.Controls.Add(this.txbPassword);
+            this.gbLogin.Controls.Add(this.label3);
+            this.gbLogin.Controls.Add(this.ckbShowPass);
+            this.gbLogin.Controls.Add(this.txbUsername);
+            this.gbLogin.Controls.Add(this.label2);
+            this.gbLogin.Location = new System.Drawing.Point(-49, 142);
+            this.gbLogin.Name = "gbLogin";
+            this.gbLogin.Size = new System.Drawing.Size(1036, 277);
+            this.gbLogin.TabIndex = 3;
+            this.gbLogin.TabStop = false;
+            // 
+            // lbTrangthai
+            // 
+            this.lbTrangthai.AutoSize = true;
+            this.lbTrangthai.BackColor = System.Drawing.Color.White;
+            this.lbTrangthai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTrangthai.ForeColor = System.Drawing.Color.Red;
+            this.lbTrangthai.Location = new System.Drawing.Point(424, 198);
+            this.lbTrangthai.Name = "lbTrangthai";
+            this.lbTrangthai.Size = new System.Drawing.Size(0, 20);
+            this.lbTrangthai.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(665, 159);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(127, 24);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Nhớ mật khẩu";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(671, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 43);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Hủy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btDangNhap
+            // 
+            this.btDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDangNhap.Location = new System.Drawing.Point(516, 228);
+            this.btDangNhap.Name = "btDangNhap";
+            this.btDangNhap.Size = new System.Drawing.Size(135, 43);
+            this.btDangNhap.TabIndex = 11;
+            this.btDangNhap.Text = "Đăng Nhập!";
+            this.btDangNhap.UseVisualStyleBackColor = true;
+            this.btDangNhap.Click += new System.EventHandler(this.btDangNhap_Click);
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbPassword.Location = new System.Drawing.Point(428, 121);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(364, 22);
+            this.txbPassword.TabIndex = 8;
+            this.txbPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(248, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Mật khẩu:";
+            // 
+            // ckbShowPass
+            // 
+            this.ckbShowPass.AutoSize = true;
+            this.ckbShowPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbShowPass.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ckbShowPass.Location = new System.Drawing.Point(428, 159);
+            this.ckbShowPass.Name = "ckbShowPass";
+            this.ckbShowPass.Size = new System.Drawing.Size(131, 24);
+            this.ckbShowPass.TabIndex = 9;
+            this.ckbShowPass.Text = "Hiện mật khẩu";
+            this.ckbShowPass.UseVisualStyleBackColor = true;
+            // 
+            // txbUsername
+            // 
+            this.txbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUsername.Location = new System.Drawing.Point(428, 82);
+            this.txbUsername.Name = "txbUsername";
+            this.txbUsername.Size = new System.Drawing.Size(364, 22);
+            this.txbUsername.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(245, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tên đăng nhập:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(885, 539);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Design by VanQuan";
+            // 
+            // frLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gbLogin);
+            this.Controls.Add(this.label1);
+            this.Name = "frLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbLogin;
+        private System.Windows.Forms.Label lbTrangthai;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btDangNhap;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox ckbShowPass;
+        private System.Windows.Forms.TextBox txbUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
